f03d058f124767222c2127a4ce4cfd24d3c96d14
Nikola Metulev <nikolame@microsoft.com>
2018-05-12 17:32:17 -0700

    working alarm

--- AlarmML/MainPage.xaml.cs
+++ AlarmML/MainPage.xaml.cs
@@ -41,28 +41,76 @@ namespace AlarmML
 
         InkshapesModel model;
 
-        DispatcherTimer timer;
+        DispatcherTimer timeTimer;
         bool alarmOn = true;
 
-        DateTime? lastTimeEmotionMatched;
+        InkDrawingAttributes ida;
+        Random random = new Random((int)DateTime.Now.Ticks);
+
+        MediaElement mediaElement;
+
+        private List<string> shapeLabels = new List<string>()
+        {
+            "airplane",
+            "axe", 
+            "bike", 
+            "bird", 
+            "bomb", 
+            "cake", 
+            "car",
+            "cat",
+            "chair",      
+            "doughnut",
+            "duck", 
+            "fish",   
+            "flower", 
+            "guitar", 
+            "heart",
+            "house",
+            "poop",  
+            "rocket", 
+            "shoe",       
+            "stick_figure",
+            "sun",
+        };
+
+        private string currentShape;
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
+            currentShape = shapeLabels[random.Next(shapeLabels.Count)];
+
+            timeTimer = new DispatcherTimer();
+            timeTimer.Interval = TimeSpan.FromMilliseconds(100);
+            timeTimer.Tick += Timer_Tick;
+            timeTimer.Start();
+
             // load Model
             var file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///model.onnx"));
             model = await InkshapesModel.CreateInkshapesModel(file);
+
+            var alarmFile = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///wakeup.m4a"));
+            var stream = await alarmFile.OpenAsync(FileAccessMode.Read);
+
+            mediaElement = new MediaElement();
+            mediaElement.IsLooping = true;
+            mediaElement.AutoPlay = true;
+            mediaElement.SetSource(stream, alarmFile.ContentType);
 
             Inker.InkPresenter.InputDeviceTypes =
             CoreInputDeviceTypes.Pen |
             CoreInputDeviceTypes.Touch |
             CoreInputDeviceTypes.Mouse;
 
+            ida = InkDrawingAttributes.CreateForPencil();
+            ida.Size = new Size(30, 30);
+            ida.Color = Colors.White;
+            ida.PencilProperties.Opacity = 1;
+            Inker.InkPresenter.UpdateDefaultDrawingAttributes(ida);
+
             Inker.InkPresenter.StrokesCollected += InkPresenter_StrokesCollectedAsync;
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromMilliseconds(300);
-            timer.Tick += Timer_Tick;
-            timer.Start();
+            SubText.Text = $"draw {currentShape} to Snooze";
         }
 
         private async void InkPresenter_StrokesCollectedAsync(InkPresenter sender, InkStrokesCollectedEventArgs args)
@@ -75,25 +123,71 @@ namespace AlarmML
             var output = await model.EvaluateAsync(input);
 
             var guessedTag = output.classLabel.First();
-            var guessedPercentage = output.loss.OrderByDescending(kv => kv.Value).First().Value.ToString();
+            var guessedPercentage = output.loss.OrderByDescending(kv => kv.Value).First().Value;
 
-            GuessText.Text = $"Current Guess: {guessedTag}({guessedPercentage})";
+            if (guessedPercentage < 0.8)
+            {
+                SubText.Text = $"draw {currentShape} to Snooze - don't know what that is";
+            }
+            else if (guessedTag != currentShape)
+            {
+                SubText.Text = $"draw {currentShape} to Snooze - you drew {guessedTag}";
+            }
+            else
+            {
+                alarmOn = false;
+            }
+
+            Debug.WriteLine($"Current guess: {guessedTag}({guessedPercentage})");
         }
 
         // decide if the alarm should be on
         private void Timer_Tick(object sender, object e)
         {
+            var now = DateTime.Now;
             if (alarmOn)
             {
-                Alarmbackground.Visibility = Alarmbackground.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+                if (now.Millisecond < 500)
+                {
+                    TimeText.Foreground = new SolidColorBrush(Color.FromArgb(0x44, 0xFF, 0, 0));
+                }
+                else
+                {
+                    TimeText.Foreground = new SolidColorBrush(Color.FromArgb(0x33, 0x33, 0x33, 0x33));
+                }
             }
             else
             {
-                Alarmbackground.Visibility = Visibility.Collapsed;
+                TimeText.Foreground = new SolidColorBrush(Color.FromArgb(0x33, 0x33, 0x33, 0x33));
+                SubText.Text = $"Good Morning!";
+                mediaElement.Stop();
             }
 
-            TimeText.Text = DateTime.Now.ToShortTimeString();
+            foreach (var stroke in Inker.InkPresenter.StrokeContainer.GetStrokes())
+            {
+                if (stroke.DrawingAttributes.PencilProperties.Opacity < 0.02)
+                {
+                    stroke.Selected = true;
+                }
+                else
+                {
+                    var attributes = stroke.DrawingAttributes;
+                    attributes.PencilProperties.Opacity -= 0.02;
+                    stroke.DrawingAttributes = attributes;
+                }
+            }
+
+            Inker.InkPresenter.StrokeContainer.DeleteSelected();
+
+            TimeText.Text = now.ToShortTimeString();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!alarmOn) alarmOn = true;
+
+            currentShape = shapeLabels[random.Next(shapeLabels.Count)];
+            SubText.Text = $"draw {currentShape} to Snooze";
+        }
     }
 }
